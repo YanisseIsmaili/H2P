0fa3d104a36132e8bc3670b9795f4f32f2d01c93
FORUM <yanisse.ismaili@ynov.com>
2023-12-04 15:39:34 +0100

    test

---
+++ arithmetic-operators.php
@@ -0,0 +1,18 @@
+<?php
+
+$firstNbr = 100;
+$secondNbr = 35;
+
+$firstSum = $firstNbr + $secondNbr;
+$firstSub = $firstNbr - $secondNbr;
+$firstMult = $firstNbr * $secondNbr;
+$firstDivision = $firstNbr / $secondNbr;
+$firstModulus = $firstNbr % $secondNbr;
+
+echo "firstSum: $firstSum\n";
+echo "firstSub: $firstSub\n";
+echo "firstMult: $firstMult\n";
+echo "firstDivision: $firstDivision\n";
+echo "firstModulus: $firstModulus\n";
+
+?>
