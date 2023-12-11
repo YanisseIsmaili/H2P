614322b78a37a95bafb2167dc8378894c6001677
FORUM <yanisse.ismaili@ynov.com>
2023-12-11 12:04:07 +0100

    zebi1

--- string-manipulator.php
+++ string-manipulator.php
@@ -1,29 +1,34 @@
 <?php
 
+// Function to capitalize the entire string
 function capsMe(string $str): string {
     return strtoupper($str);
 }
 
+// Function to convert the entire string to lowercase
 function lowerMe(string $str): string {
     return strtolower($str);
 }
 
+// Function to capitalize the first letter of each word
 function upperCaseFirst(string $str): string {
     return ucwords($str);
 }
 
+// Function to lowercase the first letter of each word
 function lowerCaseFirst(string $str): string {
-    return lcfirst(ucwords($str));
+    return lcfirst(strtolower($str));
 }
 
+// Function to remove spaces before the first word and after the last word
 function removeBlankSpace(string $str): string {
     return trim($str);
 }
 
 // Test the functions
 $string = "   hello world   ";
-echo capsMe($string) . PHP_EOL;          
-echo lowerMe($string) . PHP_EOL;         
-echo upperCaseFirst($string) . PHP_EOL;  
-echo lowerCaseFirst($string) . PHP_EOL;  
-echo removeBlankSpace($string) . PHP_EOL; 
+echo capsMe($string) . PHP_EOL;          // Output: "   HELLO WORLD   "
+echo lowerMe($string) . PHP_EOL;         // Output: "   hello world   "
+echo upperCaseFirst($string) . PHP_EOL;  // Output: "   Hello World   "
+echo lowerCaseFirst($string) . PHP_EOL;  // Output: "   hello world   "
+echo removeBlankSpace($string) . PHP_EOL; // Output: "hello world"
