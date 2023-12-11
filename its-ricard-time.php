954dec8ee30d648b961d1a511b9867fa5cddd061
FORUM <yanisse.ismaili@ynov.com>
2023-12-11 11:26:43 +0100

    zebi1

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -1,18 +1,21 @@
 <?php
-$nbrRicard = 1;
+
+$nbrRicard = 1; // Initialize the variable
 
 do {
-    echo "Come on ricard number $nbrRicard\n";
+    // Display message before each ricard
+    echo "Come on ricard number " . $nbrRicard . PHP_EOL;
 
+    // Check conditions for special messages
     if ($nbrRicard == 3) {
-        echo "I'll have to stop soon!\n";
-        break;
+        echo "I'll have to stop soon!" . PHP_EOL;
     } elseif ($nbrRicard == 5) {
-        echo "I am no longer very fresh...\n";
-        break;
+        echo "I am no longer very fresh ..." . PHP_EOL;
+    } elseif ($nbrRicard == 7) {
+        break; // Stop the loop at the 7th ricard
     }
 
+    // Increment the number of ricard
     $nbrRicard++;
 
 } while ($nbrRicard <= 7);
-?>
