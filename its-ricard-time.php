6e0d644672d28eff77ff5186ab249e85f8d09f04
FORUM <yanisse.ismaili@ynov.com>
2023-12-11 10:43:10 +0100

    zebi1

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -6,14 +6,12 @@ do {
 
     if ($nbrRicard == 3) {
         echo "I'll have to stop soon!\n";
+        break;
     } elseif ($nbrRicard == 5) {
         echo "I am no longer very fresh...\n";
     }
 
     $nbrRicard++;
 
-    if ($nbrRicard == 8) {
-        break;
-    }
-} while (true);
+} while ($nbrRicard <= 7);
 ?>
