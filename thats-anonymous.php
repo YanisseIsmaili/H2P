74864c5aaf81819696cdf1cbbe7d989f6163ec50
iyanisse <yanisse.ismaili@ynov.com>
2024-01-09 15:39:33 +0100

    et ho et ho on rentre du boulu

---
+++ thats-anonymous.php
@@ -0,0 +1,23 @@
+<?php
+
+$today = function() {
+    $dayNumber = date("j");
+    $month = date("F");
+    $year = date("Y");
+    $result = 'It is ' . $month . ' ' . $dayNumber . ', ' . $year;
+    return $result;
+};
+
+$isLeapYear = function($yearl){
+    if ($yearl % 400 == 0)
+        return True;
+    else if ($yearl % 100 == 0)
+        return False;
+    else if ($yearl % 4 == 0)
+        return True;
+    else
+        return False;
+};
+
+
+?>
