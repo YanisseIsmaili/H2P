de26922d2a0b067faa349b9d6f3418e641efb213
iyanisse <yanisse.ismaili@ynov.com>
2024-01-09 16:34:25 +0100

    et ho et ho on rentre du boulu

---
+++ join.php
@@ -0,0 +1,19 @@
+<?php
+
+function joinWords(array $tab, string $separator = ' '): string {
+
+    $counter = 0;
+    $result = '';
+    foreach($tab as $word){
+        if ($counter < 1){
+            $result .= $word;
+            $counter += 1;
+        }else{
+            $result .= $separator . $word;
+        }
+    }
+    return $result;
+
+}
+
+?>
