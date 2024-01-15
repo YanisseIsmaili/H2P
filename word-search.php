b0f0adba8245fb0bf29c476880abb5b7d37cf6cb
iyanisse <yanisse.ismail@ynov.com>
2024-01-15 17:31:11 +0100

    zebi10

--- word-search.php
+++ word-search.php
@@ -1,5 +1,20 @@
 <?php
 function searchWord(array $board,string $word) : bool {
+    $split_word = str_split($word);
+    $cpt = 0;
+    if (array_unique($split_word) !== $split_word) {
+        return false;
+    }
+    $board = array_merge(...$board);
+    foreach($split_word as $letter){
+        if (in_array($letter, $board)) {
+            $cpt += 1;
+        }
+    }
+    if ($cpt === strlen($word)) {
+        return true;
+    }
+}
 
 $board = [
     ['a', 'b', 'c', 'd'],
