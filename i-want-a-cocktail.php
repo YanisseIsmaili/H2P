1063897049382afa7469e468e95b727450646ced
iyanisse <yanisse.ismail@ynov.com>
2024-01-15 17:50:25 +0100

    zebi10

---
+++ i-want-a-cocktail.php
@@ -0,0 +1,14 @@
+<?php
+    class Mojito
+    {
+        public float $alcoholRate;
+        public array $ingredients;
+        public int $price;
+
+        public function __construct(float $alcoholRate = 0.15, array $ingredients = ["rum", "lime", "sparkling water", "mint", "sugar"], int $price = 8)
+        {
+            $this->alcoholRate = $alcoholRate;
+            $this->ingredients = $ingredients;
+            $this->price = $price;
+        }
+    }
