fb4891ae15b2755ac2947f31a84ce925d40a6bf9
iyanisse <yanisse.ismail@ynov.com>
2024-01-16 14:54:02 +0100

    zebi10

--- this-is-magic.php
+++ this-is-magic.php
@@ -1,11 +1,37 @@
 <?php
-    class Magic
-    {
-        public string $card;
-        
-        public function __construct(string $card = "As")
+        class Magic
         {
+            public string $card;
 
-            echo "initialise l'object instancié"
+            public function __construct(string $card = "As")
+            {
+                echo "initialise l'objet instancié\n";
+                $this->card = $card;
+            }
+
+            public function __toString(): string
+            {
+                echo "convertir l'objet en string\n";
+                return $this->card;
+            }
+
+            public function __destruct()
+            {
+                echo "détruire l'objet instancié\n";
+            }
+
+            public function __get($name)
+            {
+                echo "permet d'acceder a un attribut non accessible\n";
+            }
+
+            public function __set($name, $value)
+            {
+                echo "permet de modifier un attribut non accessible\n";
+            }
+
+            public function __isset($name)
+            {
+                echo "permet de savoir si un attribut non accessible existe\n";
+            }
         }
-    }
