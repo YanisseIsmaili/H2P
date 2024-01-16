c768cf830eda47c9e4c9e5c964385959e8698efd
iyanisse <yanisse.ismail@ynov.com>
2024-01-16 15:12:01 +0100

    zebi10

--- geometry-master.php
+++ geometry-master.php
@@ -5,4 +5,26 @@
         abstract public function perimeter(): int|float;
 
     }
+
+    class Rectangle extends AbstractGeometry
+    {
+        private int|float $width;
+        private int|float $height;
+
+        public function __construct(int|float $width = 0, int|float $height = 0)
+        {
+            $this->width = $width;
+            $this->height = $height;
+        }
+
+        public function area(): int|float
+        {
+            return $this->width * $this->height;
+        }
+
+        public function perimeter(): int|float
+        {
+            return ($this->width + $this->height) * 2;
+        }
+    }
     
