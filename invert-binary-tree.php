efb8a2d059e2a700d381117145b50302d45f6dc3
iyanisse <yanisse.ismail@ynov.com>
2024-01-16 15:51:48 +0100

    zebi10

--- invert-binary-tree.php
+++ invert-binary-tree.php
@@ -1,1 +1,12 @@
 <?php
+    function invertTree ($BinaryNode): BinaryNode|null
+    {
+        if ($BinaryNode === null) {
+            return null;
+        }
+
+        //permet d'appliquer la fonction invertTree sur chaque noeud de l'arbre
+        list($BinaryNode->left, $BinaryNode->right) = [invertTree($BinaryNode->right), invertTree($BinaryNode->left)];
+
+        return $BinaryNode;
+    }
